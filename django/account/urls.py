479e2b838632afa24f89c7e02c1d98b466ed5ec1
alipy-dw <ebaka646@gmail.com>
2025-06-07 10:07:29 +0600

    commit

---
+++ django/account/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from account.views import RegisterUserAPIView
+
+urlpatterns = [
+    path('register/', RegisterUserAPIView.as_view()),
+    path('login/', TokenObtainPairView.as_view()),
+    path('refresh/', TokenRefreshView.as_view())
+]
--- django/core/settings.py
+++ django/core/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
 
     # libs
     'rest_framework',
+    'rest_framework_simplejwt',
 
     # apps
     'post',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
